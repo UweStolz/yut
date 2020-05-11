8451a0e352770cb2405938b27205d2129d7ab36d
UweStolz <uwe.stolz@protonmail.com>
2020-05-11 10:00:54 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/no-extraneous-dependencies': ['error',
+      {
+        devDependencies: true, optionalDependencies: true, peerDependencies: true,
+      },
+    ],
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    'max-len': [
+      0,
+      {
+        code: 100, ignoreStrings: true,
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: [
+          '.js',
+          '.ts',
+        ],
+      },
+    },
+  },
+};
