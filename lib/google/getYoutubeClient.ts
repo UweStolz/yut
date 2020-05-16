f1b0cc9d47c93e8b9fe75d9bb1e8ec82adfecb62
UweStolz <uwe.stolz@protonmail.com>
2020-05-16 13:14:00 +0200

    feat: get YT client

---
+++ lib/google/getYoutubeClient.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { google, youtube_v3 } from 'googleapis';
+
+function decryptApiKey(): string {
+  const key = 'QUl6YVN5QlBLRVMxRGlSTUIyQlUyR1JHTmVpRVktOHpQZVlFdVRv';
+  const encryptedKey = Buffer.from(key, 'base64').toString('ascii');
+  return encryptedKey;
+}
+
+export default function getYoutubeClient(): youtube_v3.Youtube {
+  const auth = decryptApiKey();
+  const youtubeClient = google.youtube({
+    version: 'v3',
+    auth,
+  });
+  return youtubeClient;
+}
