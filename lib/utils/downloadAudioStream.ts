05c1618c339e20267dc59a720c1582313d12df9b
UweStolz <uwe.stolz@protonmail.com>
2020-05-17 12:02:49 +0200

    fix: wrong tmp path

--- lib/utils/downloadAudioStream.ts
+++ lib/utils/downloadAudioStream.ts
@@ -1,19 +1,14 @@
 import { createWriteStream } from 'fs';
-import { join } from 'path';
-import { tmpdir } from 'os';
 import { tmpNameSync } from 'tmp';
 import ytdl, { downloadOptions } from 'ytdl-core';
 
-const tmpDirFromOS = tmpdir();
-
 function getTmpFilePath(): string {
   const randomName = tmpNameSync();
-  const tmpFileName = `${randomName}.mp4`;
-  const tmpFilePath = join(tmpDirFromOS, tmpFileName); // `${tmpDirFromOS}/${tmpFileName}`;
+  const tmpFilePath = `${randomName}.mp4`;
   return tmpFilePath;
 }
 
-export default function downloadAudioStream(id: string) {
+export default function downloadAudioStream(id: string): void {
   const url = `http://www.youtube.com/watch?v=${id}`;
   const options: downloadOptions = {
     filter: 'audioonly',
@@ -22,7 +17,11 @@ export default function downloadAudioStream(id: string) {
   try {
     const tmpFilePath = getTmpFilePath();
     const audioStream = ytdl(url, options);
-    audioStream.pipe(createWriteStream(tmpFilePath));
+    const writeStream = createWriteStream(tmpFilePath, {
+      autoClose: true,
+      emitClose: true,
+    });
+    audioStream.pipe(writeStream);
   } catch (err) {
     console.error(err);
   }
