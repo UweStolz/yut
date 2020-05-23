96cf3784a1a80d1b594c0c8c1cb287b671b5266a
UweStolz <uwe.stolz@protonmail.com>
2020-05-23 13:00:27 +0200

    ref: remvoe env variable for electron

--- lib/setEnvironment.ts
+++ lib/setEnvironment.ts
@@ -1,4 +1,3 @@
-process.env.ELECTRON_RUN_AS_NODE = 'true';
 process.env.ELECTRON_NO_ATTACH_CONSOLE = 'true';
 process.env.ELECTRON_ENABLE_LOGGING = 'false';
 process.env.ELECTRON_LOG_ASAR_READS = 'false';
