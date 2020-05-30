61636ea8e565af0aaaaebbd6a3416acbea600abc
UweStolz <uwe.stolz@protonmail.com>
2020-05-30 14:34:06 +0200

    feat: get meta data of playing file

--- lib/electron/renderer/index.ts
+++ lib/electron/renderer/index.ts
@@ -1,11 +1,31 @@
-import { Howl } from 'howler';
+import { ipcRenderer } from 'electron';
+import { Howl, Howler } from 'howler';
 
+Howler.usingWebAudio = true;
 let sound: Howl;
 let soundId: undefined|number;
+
+function getFrequencyData(): any {
+  const analyser = Howler.ctx.createAnalyser();
+  Howler.masterGain.connect(analyser);
+  analyser.fftSize = 64;
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+  analyser.getByteFrequencyData(dataArray);
+  setInterval(() => {
+    if (sound.playing()) {
+      analyser.getByteTimeDomainData(dataArray);
+      const data = dataArray.map((entry: number) => entry / 2);
+      ipcRenderer.send('analyser', data);
+    }
+  }, 1000);
+}
+
 
 function initializeSound(src: string): void {
   sound = new Howl({
     src,
+    html5: false,
   });
 }
 
@@ -13,17 +33,24 @@ export function playAudio(src: string): void {
   if (!sound) {
     initializeSound(src);
   }
-  soundId = sound.play();
+  if (!sound.playing()) {
+    soundId = sound.play();
+    sound.on('play', () => {
+      getFrequencyData();
+    });
+  }
 }
 
 export function pauseAudio(): void {
   if (soundId) {
     sound.pause(soundId);
+    clearInterval();
   }
 }
 
 export function stopAudio(): void {
   if (soundId) {
     sound.stop(soundId);
+    clearInterval();
   }
 }
