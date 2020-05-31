b14d4f9fcee81798ac1e5c3aaae58fdffa90d648
UweStolz <uwe.stolz@protonmail.com>
2020-05-31 17:49:12 +0200

    ref: replace package

--- lib/terminal/utils/getLastLogEntries.ts
+++ lib/terminal/utils/getLastLogEntries.ts
@@ -1,16 +1,46 @@
-// @ts-ignore
-import readLastLines from 'read-last-lines';
 import { EOL } from 'os';
 import { Widgets } from 'blessed';
+import { readFileSync, existsSync, writeFileSync } from 'fs';
+
+function writeNewFile(data: string[]): void {
+  try {
+    const fileWithNewlines = data.map((line) => line + EOL);
+    const cleanFile = fileWithNewlines.toString().replace(/,/g, '');
+    writeFileSync('history.log', cleanFile);
+  } catch {
+    // Do nothing
+  }
+}
+
+function getLogFile(): string[] {
+  let splitFileText: string[] = [];
+  try {
+    const fileBuffer = readFileSync('history.log');
+    const fileText = fileBuffer.toString();
+    splitFileText = fileText.split(EOL);
+    const lineCount = splitFileText.length - 1;
+    if (lineCount > 100) {
+      const startPoint = lineCount - 101;
+      const reducedLog = splitFileText.splice(startPoint);
+      splitFileText = reducedLog;
+      writeNewFile(splitFileText);
+    }
+  } catch {
+    // Do nothing
+  }
+  return splitFileText;
+}
 
 export default async function getLastLogEntries(searchLog: Widgets.Log): Promise<void> {
   try {
-    const lastLogEntries = await readLastLines.read('history.log', 25) as string;
-    const lastLines = lastLogEntries.split(EOL);
-    lastLines.forEach((line) => {
-      const trimmedEntry = line.trim();
-      searchLog.add(trimmedEntry);
-    });
+    const doesFileExist = existsSync('history.log');
+    if (doesFileExist) {
+      const lastLines = getLogFile();
+      lastLines.forEach((line) => {
+        const trimmedEntry = line.trim();
+        searchLog.add(trimmedEntry);
+      });
+    }
   } catch {
     // Do nothing
   }
