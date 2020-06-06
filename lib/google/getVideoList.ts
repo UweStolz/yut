9a32c30537d9a453dcb22792bebcfef7a0c19b12
UweStolz <uwe.stolz@protonmail.com>
2020-06-06 13:22:59 +0200

    ref: adjust api calls

--- lib/google/getVideoList.ts
+++ lib/google/getVideoList.ts
@@ -10,12 +10,12 @@ export interface SearchResult extends Array<string> {
 
 const youtube = getYoutubeClient();
 
-async function getVideoDurations(id: string): Promise<string[]> {
+async function getVideoDurations(id: string[]): Promise<string[]> {
   let durations: string[] = [];
   try {
     const { data } = await youtube.videos.list({
       id,
-      part: 'contentDetails',
+      part: ['contentDetails'],
     });
     durations = data.items?.map((item) => {
       const isoDuration = item.contentDetails?.duration;
@@ -34,10 +34,10 @@ async function getVideoList(query: string): Promise<youtube_v3.Schema$SearchList
     const { data } = await youtube.search.list({
       q: query,
       fields: 'items(id(videoId),snippet(title))',
-      type: 'video',
+      type: ['video'],
       videoCategoryId: '10', // music
       maxResults: 35,
-      part: 'snippet',
+      part: ['snippet'],
     });
     result = data;
   } catch (err) {
@@ -48,8 +48,8 @@ async function getVideoList(query: string): Promise<youtube_v3.Schema$SearchList
 
 export default async function performSearch(query: string): Promise<SearchResult[]> {
   const videoList = await getVideoList(query);
-  const ids = videoList.items?.map((entry) => entry.id?.videoId).toString();
-  const durations = await getVideoDurations(ids as string);
+  const ids = videoList.items?.map((entry) => entry.id?.videoId);
+  const durations = await getVideoDurations(ids as string[]);
   const searchResult = videoList.items?.map((item, index) => {
     const currentVideo = [
         item.id?.videoId,
