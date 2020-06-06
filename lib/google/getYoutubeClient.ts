fc13197ca8ab1d4ab1cace52e3158410f8267fd5
UweStolz <uwe.stolz@protonmail.com>
2020-06-06 17:51:19 +0200

    ref: require dotenv asap

--- lib/google/getYoutubeClient.ts
+++ lib/google/getYoutubeClient.ts
@@ -1,6 +1,5 @@
 /* eslint-disable camelcase */
 import { google, youtube_v3 } from 'googleapis';
-import 'dotenv/config';
 
 export default function getYoutubeClient(): youtube_v3.Youtube {
   const auth = process.env.YT_API_KEY;
--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import './setEnvironment';
 import './electron/main';
 import createScreen from './terminal';
